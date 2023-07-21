e457a778b05a00aa923785001318f375406cf117
guodongAndroid <sun33919135@gmail.com>
2023-07-21 09:08:13 +0000

    Deploying to deploy from @ qingdao-kug/qingdao-kug.github.io@ffa163d502d47e2999f7f5c717f5e4c32ca853e2 🚀

---
+++ assets/start_index.md.8b4a2ba4.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,U as l}from"./chunks/framework.76b79cb5.js";const F=JSON.parse('{"title":"基本语法","description":"","frontmatter":{},"headers":[],"relativePath":"start/index.md","filePath":"start/index.md"}'),o={name:"start/index.md"},p=l("",35),t=[p];function e(c,r,C,A,i,y){return a(),n("div",null,t)}const u=s(o,[["render",e]]);export{F as __pageData,u as default};
